0b52aa567690e1441a16620cc819fb696fcda49c
Jayson Sam Fong <fcr808investments@gmail.com>
2018-01-25 05:00:15 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -8,7 +8,7 @@ if (index.name === "Susan") {
 var index = require("./index.js")
 
 
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
